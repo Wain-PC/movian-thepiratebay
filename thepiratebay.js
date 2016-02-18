fba618dd8818646c574f523f7490c6a96e0215d7
Wain <wain-pc@mail.ru>
2016-02-18 09:15:17 +0300

    -Added search and pagination -Code optimizations

--- thepiratebay.js
+++ thepiratebay.js
@@ -1,5 +1,5 @@
 /**
- * thepiratebay.se plugin for Showtime
+ * ThePirateBay plugin for Showtime
  *
  *  Copyright (C) 2014-2016 Wain
  *
@@ -21,18 +21,7 @@
     var config = {
         pluginInfo: plugin.getDescriptor(),
         prefix: plugin.getDescriptor().id,
-        logo: plugin.path + "logo.png",
-        headers: {
-            Connection: "keep-alive",
-            Pragma: "no-cache",
-            "Cache-Control": "no-cache",
-            Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": 1,
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36",
-            "Accept-Encoding": "gzip, deflate, sdch",
-            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,und;q=0.2"
-        },
-        regExps: {}
+        logo: plugin.path + "logo.png"
     };
 
     var service = plugin.createService(config.pluginInfo.title, config.prefix + ":start", "video", true, config.logo);
@@ -40,17 +29,15 @@
     var html = require('showtime/html');
     settings.createInfo("info", config.logo, "Plugin developed by " + config.pluginInfo.author + ". \n");
     settings.createDivider('Settings');
-    settings.createString("domain", "Домен", "thepiratebay.se", function (v) {
+    settings.createString("domain", "Domain", "thepiratebay.se", function (v) {
         service.domain = v;
     });
+    var nextUrlsRe = /<a href="([\s\w\/]*?)"><img[\s\S]{0,70}?alt="Next"\/?><\/a>/m;
 
     config.urls = {
         base: 'http://' + service.domain
     };
 
-    function coloredStr(str, color) {
-        return '<font color="' + color + '">' + str + '</font>';
-    }
 
     function setPageHeader(page, title) {
         if (page.metadata) {
@@ -67,34 +54,29 @@
         var pages = [
                 {
                     url: '/browse',
-                    path: 'category',
                     name: 'Browse'
                 },
                 {
                     url: '/recent',
-                    path: 'items',
                     name: 'Recent'
                 },
                 {
                     url: '/tv',
-                    path: 'category',
                     name: 'TV Shows'
                 },
                 {
                     url: '/music',
-                    path: 'category',
                     name: 'Music'
                 },
                 {
                     url: '/top',
-                    path: 'category',
                     name: 'Top 100'
                 }
             ],
             i, length = pages.length;
         setPageHeader(page, config.pluginInfo.synopsis);
         for (i = 0; i < length; i++) {
-            page.appendItem(config.prefix + ":" + pages[i].path + ":" + pages[i].url + ':' + encodeURIComponent(pages[i].name), "directory", {
+            page.appendItem(config.prefix + ":category:" + pages[i].url + ':' + encodeURIComponent(pages[i].name), "directory", {
                 title: pages[i].name
             });
         }
@@ -102,56 +84,70 @@
 
 
     plugin.addURI(config.prefix + ":category:(.*):(.*)", function (page, url, name) {
-        var dom, items, i, itemUrl;
+        var dom, doc, items, i,
+            link, linkUrl, linkTitle,
+            nextUrl, prefix,
+            tryToSearch = true;
         setPageHeader(page, decodeURIComponent(name));
-        page.loading = true;
-        dom = html.parse(showtime.httpReq(config.urls.base + decodeURIComponent(url)).toString());
-        page.loading = false;
+        url = config.urls.base + decodeURIComponent(url);
 
-        //1-title, 2- HTML contents
-        try {
-            items = dom.root.getElementById('categoriesTable').getElementByTagName('a');
-        }
-        catch(err) {
-           return page.redirect(config.prefix + ":items:" + url + ':' + name);
-        }
-        for (i = 0; i < items.length; i++) {
-            itemUrl = items[i].attributes.getNamedItem('href').value;
-            if (itemUrl.indexOf('browse') > -1) {
-                page.appendItem(config.prefix + ":items:" + encodeURIComponent(items[i].attributes.getNamedItem('href').value) + ':' + encodeURIComponent(items[i].textContent), "directory", {
-                    title: items[i].textContent
-                });
+        page.paginator = loader;
+        loader();
+
+        function loader() {
+            if (!tryToSearch) {
+                return false;
+            }
+
+            page.loading = true;
+            doc = showtime.httpReq(url).toString();
+            dom = html.parse(doc);
+            page.loading = false;
+
+
+            items = dom.root.getElementById('categoriesTable');
+            if (items) {
+                items = items.getElementByTagName('a');
+                prefix = 'category';
+                for (i = 0; i < items.length; i++) {
+                    link = items[i];
+                    linkUrl = items[i].attributes.getNamedItem('href').value;
+                    linkTitle = items[i].textContent;
+                    page.appendItem(config.prefix + ":category:" + encodeURIComponent(linkUrl) + ':' + encodeURIComponent(linkTitle), "directory", {
+                        title: items[i].textContent
+                    });
+                }
             }
             else {
-                page.appendItem(config.prefix + ":category:" + encodeURIComponent(items[i].attributes.getNamedItem('href').value) + ':' + encodeURIComponent(items[i].textContent), "directory", {
-                    title: items[i].textContent
-                });
+                items = dom.root.getElementById('searchResult');
+                if (items) {
+                    items = items.getElementByClassName('detName');
+                    for (i = 0; i < items.length; i++) {
+                        link = items[i].getElementByTagName('a')[0];
+                        linkUrl = link.attributes.getNamedItem('href').value;
+                        linkTitle = link.textContent;
+                        page.appendItem(config.prefix + ":torrent:" + encodeURIComponent(linkUrl) + ':' + encodeURIComponent(linkTitle), "directory", {
+                            title: linkTitle
+                        });
+                    }
+                }
+                else {
+                    return tryToSearch = false;
+                }
             }
+
+            nextUrl = nextUrlsRe.exec(doc);
+            if (!nextUrl) {
+                return tryToSearch = false;
+            }
+
+            url = config.urls.base + nextUrl[1];
+            return true;
         }
     });
-
-    plugin.addURI(config.prefix + ":items:(.*):(.*)", function (page, url, name) {
-        var dom, items, i, link, linkUrl, linkTitle;
-        setPageHeader(page, decodeURIComponent(name));
-        page.loading = true;
-        dom = html.parse(showtime.httpReq(config.urls.base + decodeURIComponent(url)).toString());
-        page.loading = false;
-
-        //1-title, 2- HTML contents
-        items = dom.root.getElementById('searchResult').getElementByClassName('detName');
-        for (i = 0; i < items.length; i++) {
-            link = items[i].getElementByTagName('a')[0];
-            linkUrl = link.attributes.getNamedItem('href').value;
-            linkTitle = link.textContent;
-            page.appendItem(config.prefix + ":torrent:" + encodeURIComponent(linkUrl) + ':' + encodeURIComponent(linkTitle), "directory", {
-                title: linkTitle
-            });
-        }
-    });
-
 
     plugin.addURI(config.prefix + ":torrent:(.*):(.*)", function (page, url, name) {
-        var dom, items, magnetUrl, torrentTitle, torrentDescription;
+        var dom, magnetUrl, torrentDescription;
         name = decodeURIComponent(name);
         setPageHeader(page, name);
         page.loading = true;
@@ -160,61 +156,56 @@
 
         magnetUrl = dom.root.getElementByClassName('download')[0].getElementByTagName('a')[0].attributes.getNamedItem('href').value;
         torrentDescription = dom.root.getElementByClassName('nfo')[0].textContent;
-        console.log(magnetUrl);
         page.appendItem("torrent:browse:" + magnetUrl, "directory", {
             title: 'magnet: ' + name,
             description: torrentDescription
         });
     });
 
-
     plugin.addSearcher(plugin.getDescriptor().id, config.logo, function (page, query) {
-        var url = config.urls.base + config.urls.parts.search + encodeURIComponent(query),
-            nextURL, tryToSearch = true;
+        //Example URL: https://thepiratebay.se/search/one/0/99/0
+        var url = config.urls.base + '/search/' + encodeURIComponent(query) + '/0/99/0',
+            nextUrl, tryToSearch = true, i,
+            doc, dom, link, linkUrl, linkTitle, items;
 
         page.entries = 0;
         loader();
         page.paginator = loader;
 
         function loader() {
-
-        }
-
-
-        function makeDescription(response) {
-            var result = {
-                    title: "",
-                    href: "",
-                    topicId: "",
-                    size: "0",
-                    seeders: "0",
-                    leechers: "0"
-                },
-            //1-номер темы, 2-относительная ссылка на тему, 3-название
-                nameMatch = config.regExps.search.name.exec(response),
-            //1-размер, 2-сидеры, 3-личеры
-                infoMatch = config.regExps.search.info.exec(response);
-
-            if (nameMatch) {
-                result.title = nameMatch[3];
-                result.href = nameMatch[2];
-                result.topicId = nameMatch[1];
+            if (!tryToSearch) {
+                return false;
             }
-            if (infoMatch) {
-                result.size = infoMatch[1];
-                result.seeders = infoMatch[2];
-                result.leechers = infoMatch[3];
-            }
-            //сформируем готовую строку с описанием торрента
-            result.description = coloredStr('Название: ', config.colors.orange) + result.title + "<br>";
-            result.description += coloredStr('Размер: ', config.colors.blue) + result.size + "<br>";
-            result.description += coloredStr('Сидеры: ', config.colors.green) + result.seeders + "<br>";
-            result.description += coloredStr('Личеры: ', config.colors.red) + result.leechers + "<br>";
-            result.description = new showtime.RichText(result.description);
-            return result;
-        }
 
+            page.loading = true;
+            doc = showtime.httpReq(url).toString();
+            dom = html.parse(doc);
+            page.loading = false;
+
+            items = dom.root.getElementById('searchResult');
+            if (items) {
+                items = items.getElementByClassName('detName');
+                for (i = 0; i < items.length; i++) {
+                    link = items[i].getElementByTagName('a')[0];
+                    linkUrl = link.attributes.getNamedItem('href').value;
+                    linkTitle = link.textContent;
+                    page.appendItem(config.prefix + ":torrent:" + encodeURIComponent(linkUrl) + ':' + encodeURIComponent(linkTitle), "directory", {
+                        title: linkTitle
+                    });
+                    page.entries++;
+                }
+            }
+            else {
+                return tryToSearch = false;
+            }
+
+            nextUrl = nextUrlsRe.exec(doc);
+            if (!nextUrl) {
+                return tryToSearch = false;
+            }
+
+            url = config.urls.base + nextUrl[1];
+            return true;
+        }
     });
-
-
 })(this);
